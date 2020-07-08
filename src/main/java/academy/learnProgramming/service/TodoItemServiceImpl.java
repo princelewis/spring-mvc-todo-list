069134a935465f4cbb67dc11dc08546bb220c9e3
princelewis <princelewis4christ@yahoo.com>
2020-07-08 15:38:08 +0100

    Modify the TodoItemService class

---
+++ src/main/java/academy/learnProgramming/service/TodoItemServiceImpl.java
@@ -0,0 +1,35 @@
+package academy.learnProgramming.service;
+
+import academy.learnProgramming.model.TodoData;
+import academy.learnProgramming.model.TodoItem;
+import lombok.Getter;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoItemServiceImpl implements TodoItemService {
+
+    @Getter
+    private final TodoData data = new TodoData();
+
+
+    @Override
+    public void addItem(TodoItem toAdd) {
+        data.addItem(toAdd);
+    }
+
+    @Override
+    public void removeItem(int id) {
+        data.removeItem(id);
+    }
+
+    @Override
+    public TodoItem getItem(int id) {
+        return data.getItem(id);
+    }
+
+    @Override
+    public void updateItem(TodoItem toUpdate) {
+        data.updateItem(toUpdate);
+    }
+
+}
